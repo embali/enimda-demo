c4749e0f7dcd08382fac7157c2b410390d38a36d
Anton Smolin <smolin.anton@gmail.com>
2016-08-05 18:33:11 +0300

    kwargs

--- demo.py
+++ demo.py
@@ -21,10 +21,11 @@ source_bordered_files = sorted(_ for _ in listdir(SOURCE_BORDERED_PATH)
 # Process bordered images
 rate = 0
 for index, name in enumerate(source_bordered_files):
-    image = ENIMDA(path=join(SOURCE_BORDERED_PATH, name), mode='L', resize=300)
+    image = ENIMDA(file_=join(SOURCE_BORDERED_PATH, name), mode='L',
+                   resize=300)
     image.scan(threshold=0.5, indent=0.25)
     rate += int(image.has_borders)
-    image.save(path=join(DETECTED_BORDERED_PATH, name), outline=True)
+    image.save(file_=join(DETECTED_BORDERED_PATH, name), outline=True)
     print(index, name, image.has_borders, image.borders)
 
 print(rate / len(source_bordered_files))
@@ -32,10 +33,10 @@ print(rate / len(source_bordered_files))
 # Process clear images
 rate = 0
 for index, name in enumerate(source_clear_files):
-    image = ENIMDA(path=join(SOURCE_CLEAR_PATH, name), mode='L', resize=300)
+    image = ENIMDA(file_=join(SOURCE_CLEAR_PATH, name), mode='L', resize=300)
     image.scan(threshold=0.5, indent=0.25)
     rate += int(image.has_borders)
-    image.save(path=join(DETECTED_CLEAR_PATH, name), outline=True)
+    image.save(file_=join(DETECTED_CLEAR_PATH, name), outline=True)
     print(index, name, image.has_borders, image.borders)
 
 print(rate / len(source_clear_files))
