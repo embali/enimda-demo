38f709c0a45f46449fc0ef63b15f95a34fa17b90
Anton Smolin <smolin.anton@gmail.com>
2017-05-09 02:18:35 +0300

    bump enimda version, readme

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Demonstration for [ENIMDA](https://github.com/embali/enimda/) library
 ## Setup environment and packages
 
 ```
-pyvenv-3.5 .env
+python-3.6 -m venv .env
 
 source .env/bin/activate
 
@@ -36,9 +36,8 @@ Using included examples each minimized to 300px and default threshold value (0.5
 detection rate for bordered images is 81.2%, false detection rate for clear images
 is 6.9%
 
-With parameter stripes set to 0.05 (5%) detection becomes 10 times faster and these
-rates are 74-78% and 6-7% respectively
+With parameter columns set to 0.05 (5%) detection becomes times faster and these
+rates are 74-78% and 6-8% respectively
 
-Please be notified that using stripes (max_stripes) or frames (max_frames)
-parameters may lead to unstable result for borders attribute - it would depend
-on image nature and structure
+Please be notified that using columns or frames parameters may lead to unstable
+result for border detection - it would depend on image nature and structure
--- demo.py
+++ demo.py
@@ -20,32 +20,32 @@ source_bordered_files = sorted(_ for _ in listdir(SOURCE_BORDERED_PATH)
 
 
 SIZE = 300      # Resize to 300px
-STRIPES = 0.05  # Use 5 percent of all columns
+COLUMNS = 100   # Use 100 columns at maximum
+FAST = True     # Fast or precise scan
+
 
 # Process bordered images
 rate = 0
 for index, name in enumerate(source_bordered_files):
-    em = ENIMDA(fp=join(SOURCE_BORDERED_PATH, name), minimize=SIZE)
+    em = ENIMDA(fp=join(SOURCE_BORDERED_PATH, name), size=SIZE)
     t = time.time()
-    em.scan(stripes=STRIPES, fast=True)
+    borders = em.scan(columns=COLUMNS, fast=FAST)
     t = time.time() - t
-    rate += int(em.has_borders)
-    em.outline()
-    em.save(fp=join(DETECTED_BORDERED_PATH, name))
-    print(t, index, name, em.has_borders, em.borders)
-
+    rate += int(any(borders))
+#    em.outline()
+#    em.save(fp=join(DETECTED_BORDERED_PATH, name))
+    print(t, index, name, any(borders), borders)
 print(rate / len(source_bordered_files))
 
 # Process clear images
 rate = 0
 for index, name in enumerate(source_clear_files):
-    em = ENIMDA(fp=join(SOURCE_CLEAR_PATH, name), minimize=SIZE)
+    em = ENIMDA(fp=join(SOURCE_CLEAR_PATH, name), size=SIZE)
     t = time.time()
-    em.scan(stripes=STRIPES, fast=True)
+    borders = em.scan(columns=COLUMNS, fast=FAST)
     t = time.time() - t
-    rate += int(em.has_borders)
-    em.outline()
-    em.save(fp=join(DETECTED_CLEAR_PATH, name))
-    print(t, index, name, em.has_borders, em.borders)
-
+    rate += int(any(borders))
+#    em.outline()
+#    em.save(fp=join(DETECTED_CLEAR_PATH, name))
+    print(t, index, name, any(borders), borders)
 print(rate / len(source_clear_files))
